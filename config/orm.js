383b7b59c77555dee163eb3bc71c02c27258e832
nhuyp <ypham87@gmail.com>
2017-06-22 23:31:31 -0700

    controllers and models initial

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,20 @@
-var connection = require(connection.js);
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Object for all SQL statements:
+
+var orm = {
+    selectAll: function(){
+
+    },
+
+    insertOne: function (){
+
+    },
+
+    updateOne: function (){
+
+    }
+};
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,6 @@
+// Dependencies
+var orm = require("../../config/orm.js");
+
+// Add code to call ORM functions using burger specific input for ORM. 
+
+// Export model. 
