bcde9bb912a25a1224a8ae71865739472b02d8b3
nhuyp <ypham87@gmail.com>
2017-06-22 23:21:29 -0700

    connection.js set up

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE if not exists burgers_db;
+ 
+USE burgers_db;
+ 
+CREATE TABLE burgers (
+	id INTEGER(11) auto_increment NOT NULL, 
+    burger_name VARCHAR(30) NOT NULL, 
+    devoured BOOLEAN NOT NULL DEFAULT 0, , 
+    date timestamp(10),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO burgers 
+	(burger_name, devoured, date) 
+VALUES 
+	
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers 
+	(name, coolness_points, attitudes) 
+VALUES 
+	("Jerry Seinfeld", "10", null),
+    ("Kramer", "10", null),
+    ("Elaine Benes", "10", null), 
+    ("George Costanza", "10", null); 
