ae012374899c2fc5f3d85a3615b8840751a783c2
nhuyp <ypham87@gmail.com>
2017-06-25 16:04:51 -0700

    heroku JAWSDB

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require ("method-override");
 var path = require ("path");
 
-var PORT = process.env.port || 3000; 
+var PORT = process.env.PORT || 3000; 
 
 var app = express(); 
 
